229aaf5866ccd7bea55a294bf473fa6e7399d12a
Guilherme C <62406808+guilherme-couto@users.noreply.github.com>
2023-05-15 11:23:46 -0300

    New files

--- include/read.hpp
+++ include/read.hpp
@@ -46,58 +46,7 @@ Graph* readInstance(string input_file_address)
     string x_coord;
     string y_coord;
 
-    if (input_file == "bayg29.tsp")
-    {
-        // Skip first 3 lines
-        for (int i = 0; i < 3; i++)
-        {
-            getline(instance_file, garbage);
-        }
-
-        // Get number of nodes
-        getline(instance_file, line);
-        ss << line;
-        getline(ss, garbage);
-        pos = garbage.find(' ');
-        temp = garbage.substr(pos + 1, garbage.length());
-        number_of_nodes = stoi(temp);
-        cout << "Number of nodes: " << number_of_nodes << endl;
-
-        // Insert nodes
-        for (int i = 0; i < number_of_nodes; i++)
-        {
-            graph->insert_node(i + 1);
-        }
-
-        // Skip 4 lines
-        for (int i = 0; i < 4; i++)
-        {
-            getline(instance_file, garbage);
-        }
-
-        // Get edges from superior triangular matrix
-        for (int i = 1; i <= number_of_nodes; i++)
-        {
-            getline(instance_file, line);
-            stringstream aux_ss(line);
-            
-            for (int j = i+1; j <= number_of_nodes; j++)
-            {
-                aux_ss >> distance;
-                if (distance != " ")
-                {
-                    graph->insert_edge(i, j, stod(distance));
-                    graph->insert_edge(j, i, stod(distance));
-                }
-                else
-                {
-                    cout << "No edge from " << i << " to " << j << endl;
-                }
-            }
-        }
-        
-    }
-    else if (input_file == "ch130.tsp")
+    if (input_file == "berlin52.tsp" || input_file == "ch130.tsp")
     {
         // Skip first 3 lines
         for (int i = 0; i < 3; i++)
