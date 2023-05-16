21fb8917ebbf5331bd0e03325824ef1a9cbb5781
Patrick Carvalho <patrickcarvalho448@gmail.com>
2023-05-16 14:57:08 -0300

    início do algoritmo

--- source/ACOAlgorithm.h
+++ source/ACOAlgorithm.h
@@ -6,7 +6,23 @@
 #define TSP_EVOLUTIONARY_ACOALGORITHM_H
 
 
+#include "Graph.hpp"
+
 class ACOAlgorithm {
+private:
+    double beta;
+    double rho;
+    double q0;
+    int m;
+    double tau0;
+    int cl;
+    Graph* graph;
+    double** pheromone;
+
+public:
+    ACOAlgorithm(double beta, double rho, double q0, int m, double tau0, int cl);
+    ~ACOAlgorithm();
+    void runACO(int tMax, int seed, Graph* graph);
 
 };
 
